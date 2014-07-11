5bed280b975563f4ad7d0fc573fe0efed399c5b6
mbmartinez <mbmartinez@exist.com>
2014-07-11 18:16:19 +0800

    Stuff, things

---
+++ tyrael-process-mgmt/tyrael-process-mgmt-models/src/main/java/com/tyrael/process/mgt/models/material/Material.java
@@ -0,0 +1,10 @@
+package com.tyrael.process.mgt.models.material;
+
+import javax.persistence.Entity;
+
+import com.baldy.commons.models.BaseBaldyEntity;
+
+@Entity(name = "MATERIAL")
+public class Material extends BaseBaldyEntity {
+
+}
