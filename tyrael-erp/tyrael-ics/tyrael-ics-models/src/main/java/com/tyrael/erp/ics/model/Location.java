9c8646bf634a2d4b621c69708af550fc46af8bdb
mbmartinez <mbmartinez@exist.com>
2014-10-27 18:11:59 +0800

    wat

---
+++ tyrael-erp/tyrael-ics/tyrael-ics-models/src/main/java/com/tyrael/erp/ics/model/Location.java
@@ -0,0 +1,10 @@
+package com.tyrael.erp.ics.model;
+
+import javax.persistence.Entity;
+
+import com.baldy.commons.models.BaseEntity;
+
+@Entity(name = "LOCATION")
+public class Location extends BaseEntity {
+
+}
