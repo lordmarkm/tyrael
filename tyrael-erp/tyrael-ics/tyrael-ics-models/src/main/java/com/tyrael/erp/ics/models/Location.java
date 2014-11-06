fcdc2445a5ee0ae796520796ecc5e4ca723a2af1
mbmartinez <mbmartinez@exist.com>
2014-11-06 15:39:46 +0800

    Added parent location to Location

--- tyrael-erp/tyrael-ics/tyrael-ics-models/src/main/java/com/tyrael/erp/ics/models/Location.java
+++ tyrael-erp/tyrael-ics/tyrael-ics-models/src/main/java/com/tyrael/erp/ics/models/Location.java
@@ -1,6 +1,8 @@
 package com.tyrael.erp.ics.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 
 import com.baldy.commons.models.BaseNamedEntity;
 
@@ -9,5 +11,17 @@ import com.baldy.commons.models.BaseNamedEntity;
  */
 @Entity(name = "LOCATION")
 public class Location extends BaseNamedEntity {
+
+    @OneToMany
+    @JoinColumn(name = "PARENT_LOCATION_ID")
+    private Location parent;
+
+    public Location getParent() {
+        return parent;
+    }
+
+    public void setParent(Location parent) {
+        this.parent = parent;
+    }
 
 }
