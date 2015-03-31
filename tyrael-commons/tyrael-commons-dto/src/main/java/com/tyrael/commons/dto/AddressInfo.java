f8d3858daa762f5a356d04c9930609ebf80f2027
mbmartinez <mbmartinez@exist.com>
2015-03-31 18:27:53 +0800

    asdf

--- tyrael-commons/tyrael-commons-dto/src/main/java/com/tyrael/commons/dto/AddressInfo.java
+++ tyrael-commons/tyrael-commons-dto/src/main/java/com/tyrael/commons/dto/AddressInfo.java
@@ -1,11 +1,45 @@
 package com.tyrael.commons.dto;
 
-
+/**
+ * @author mbmartinez
+ */
 public class AddressInfo {
 
-    private String street;
+    private String addressLine1;
+    private String addressLine2;
     private String city;
     private String province;
     private String zip;
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getProvince() {
+        return province;
+    }
+    public void setProvince(String province) {
+        this.province = province;
+    }
+    public String getZip() {
+        return zip;
+    }
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
 
 }
